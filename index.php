d7b6486fc6138c1874643e4e6ca7fd4df50a8835
xaramazina2 <xaramazina2@gmail.com>
2018-11-15 16:11:04 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -20,84 +20,36 @@ elseif (!empty($_SESSION['bursar'])&&!empty($_SESSION['type'])) {
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">  
+    <link rel="stylesheet" type="text/css" href="index.css">  
 	<title>Hospital Management System - Login</title>
-	<style type="text/css">
-	body
-	{
-		background-color:green;
-	}
-		.wrapper
-		{
-			height: 250px;
-			width: 700px;
-			background-color: white;
-			border: 1px solid #C2C5BA;
-			margin: 0 auto;
-			margin-top: 150px;
-		}
-		.left
-		{
-			height: 170px;
-			width: 400px;
-			border-right: 1px solid #C2C5BA;
-			float: left;
-			font-family: Arial;
-			font-size: 25px;
-			text-align: center;
-			padding-top: 80px;
-		}
-		.right
-		{
-			height: 250px;
-			width: 299px;
-			float: left;
-			text-align: center;
-			font-family: Arial;
-		}
-		hr
-		{
-			border-bottom: 1px solid #ccc;
-			border-top: 1px solid white;
-		}
-		.input
-		{
-			height: 30px;
-			width: 80%;
-			padding-left: 20px;
-		}
-		.btn
-		{
-			height: 35px;
-			width: 90%;
-			border: 0;
-			background-color:green;
-			margin: 0;
-			color: white;
-			font-weight: bold;
-			cursor: pointer;
-		}
-	</style>
 </head>
 <body>
-<div class="wrapper">
+<div class="main_login">
+ <div class="wrapper">
 	<div class="left">
 		Hospital Management System<br><br>(HMS)
-	</div>
-	<div class="right">
-		<h3>Login Here</h3><hr>
-		<form action="index.php" method="post">
-			<input type="text" class="input" name="username" placeholder="Enter Username"><br><br>
-			<input type="password" class="input" name="password" placeholder="Enter Password"><br><br>
-			<input type="submit" class="btn" name="btn" value="Login"><br>
-		</form>
-<?php
-		extract($_POST);
-		if (isset($btn) && !empty($username) && !empty($password)) {
-			require 'includes/users.php';
-		 	login();
-		 } 
-		 ?>
+		</div>
+		<div class="right">
+			<h3>Login Here</h3><hr>
+			<form action="index.php" method="post">
+				<input type="text" class="input" name="username" placeholder="Enter Username"><br><br>
+				<input type="password" class="input" name="password" placeholder="Enter Password"><br><br>
+				<input type="submit" class="btn" name="btn" value="Login"><br>
+			</form>
+	<?php
+			extract($_POST);
+			if (isset($btn) && !empty($username) && !empty($password)) {
+				require 'includes/users.php';
+			 	login();
+			 } 
+			 ?>
+			</div>
 	</div>
 </div>
+
 </body>
 </html>
